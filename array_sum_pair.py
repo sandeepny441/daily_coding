a7008d81d7877881731b89e1af36e1ef2a1ec6ca
sandeepny441 <62777490+sandeepny441@users.noreply.github.com>
2020-03-28 21:02:05 -0500

    Update array_sum_pair.py

--- array_sum_pair.py
+++ array_sum_pair.py
@@ -1,1 +1,15 @@
 Find pair with given sum in the array
+
+def get_pair_count(nums, target_sum):
+    count = {}
+    for num in nums:
+        count[num] = count.get(num, 0) + 1
+
+    total_double = 0
+    for num in nums:
+        complement = target_sum - num
+        if complement in count:
+            total_double += count[complement]
+            if complement == num:
+                total_double -= 1
+    return total_double // 2
